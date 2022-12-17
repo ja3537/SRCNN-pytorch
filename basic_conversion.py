595e65345f81a201661807bf37dd710503a1266b
Joe Adams <78444947+ja3537@users.noreply.github.com>
2022-12-16 22:00:11 -0500

    adding more to basic_conversion

--- basic_conversion.py
+++ basic_conversion.py
@@ -8,6 +8,16 @@ from aihwkit.simulator.configs import InferenceRPUConfig
 model = SRCNN()
 rpu_config = InferenceRPUConfig()
 
+rpu_config.forward.inp_res = 1 / 64.  # 6-bit DAC discretization.
+rpu_config.forward.out_res = 1 / 256.  # 8-bit ADC discretization.
+rpu_config.forward.w_noise_type = WeightNoiseType.ADDITIVE_CONSTANT
+rpu_config.forward.w_noise = 0.02  # Some short-term w-noise.
+rpu_config.forward.out_noise = 0.02  # Some output noise.
+
+# specify the noise model to be used for inference only
+rpu_config.noise_model = PCMLikeNoiseModel(g_max=25.0)  # the model described
+
 model = convert_to_analog(model, rpu_config)
 
 save(model.state_dict(), "basic_conversion.pth")
+print("saved")
