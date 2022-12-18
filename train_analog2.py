6b11ef7c7d389841f5702324a38be6fc46cfac01
Joe Adams <78444947+ja3537@users.noreply.github.com>
2022-12-18 12:02:40 -0500

    more experiment

--- train_analog2.py
+++ train_analog2.py
@@ -64,6 +64,8 @@ if __name__ == '__main__':
 
     if cuda.is_compiled():
         model.cuda()
+    else:
+        model.cpu()
     print('if cuda is compiled:')
     print(cuda.is_compiled())
     criterion = nn.MSELoss()
