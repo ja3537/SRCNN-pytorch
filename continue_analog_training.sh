90bf326105f9e7197b2c1a11a244223a757a7934
Joe Adams <78444947+ja3537@users.noreply.github.com>
2022-12-19 20:56:01 -0500

    minor

--- continue_analog_training.sh
+++ continue_analog_training.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 lr=1e-4
 
-for prev in {3..19}
+for prev in {2..19}
 do
   python3 continue_analog_training.py --prev-epoch $prev --num-workers 0 --train-file data/91-image_x4.h5 --eval-file data/Set5_x4.h5 --scale 4 --lr $lr --num-epochs 1 --seed 42 --outputs-dir trained_models/
 done
